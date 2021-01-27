9c73d4b986acc113fd65169c80becbd2e4a9273a
alfa871212 <alfa871212@gmail.com>
2021-01-28 02:27:12 +0800

    Lecture 1 done

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by alfa_mac on 2021/1/28.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
